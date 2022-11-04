6d5389c9ebf8c922f9425c7ff3041ca194ec46dc
Dog <102476973+imD0g@users.noreply.github.com>
2022-11-04 15:28:47 +0000

    Update rock-paper-scissors.php

--- codewars/rock-paper-scissors.php
+++ codewars/rock-paper-scissors.php
@@ -1,13 +1,3 @@
-/*
-Let's play! You have to return which player won! In case of a draw return Draw!.
-
-Examples(Input1, Input2 --> Output):
-
-"scissors", "paper" --> "Player 1 won!"
-"scissors", "rock" --> "Player 2 won!"
-"paper", "paper" --> "Draw!"
-*/
-
 function rpc ($p1, $p2) {
   if ($p1 === $p2){
        return 'Draw!';
