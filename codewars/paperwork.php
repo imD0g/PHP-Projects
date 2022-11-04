6398777f15376a9087784d3b692de8082ed040ef
Dog <102476973+imD0g@users.noreply.github.com>
2022-11-04 15:06:04 +0000

    Create paperwork.php

---
+++ codewars/paperwork.php
@@ -0,0 +1,14 @@
+/*
+Problem:
+Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+*/
+
+function paperwork(int $n, int $m): int
+{
+  if ($n < 0  or $m < 0){
+    return 0;
+  } else {
+    return ($n * $m);
+  }
+}
